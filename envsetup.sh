8a097992704597d4499d4d48699007b36c1b9590
Bicet <bicets@gmail.com>
2021-03-04 08:53:32 +0000

    Check and set ccache path on envsetup

--- envsetup.sh
+++ envsetup.sh
@@ -1602,4 +1602,17 @@ validate_current_shell
 source_vendorsetup
 addcompletions
 
+# check and set ccache path on envsetup
+if [ -z ${CCACHE_EXEC} ]; then
+    ccache_path=$(which ccache)
+    if [ ! -z "$ccache_path" ]; then
+        export USE_CCACHE=1
+        export CCACHE_EXEC="$ccache_path"
+        $ccache_path -o compression=true
+        echo "ccache found and CCACHE_EXEC has been set to : $ccache_path"
+    else
+        echo "ccache not found/installed!"
+    fi
+fi
+
 export ANDROID_BUILD_TOP=$(gettop)
